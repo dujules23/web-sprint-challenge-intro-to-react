830b119fb02cc20419f1554079e33d211fd1e288
Durrell Jules <dujules23@gmail.com>
2021-01-09 18:22:37 -0500

    Successful API call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [films, setFilms] = useState([]);
+  
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/films/`)
+      .then((res) => {
+      setFilms(res.data);
+      console.log("success!", res.data);
+    })
+    .catch((error) => console.log("Error!"));
+  }, []);
+
+  console.log(films.results)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
