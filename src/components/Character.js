57b6dedf4106bd99cd94d77cce9ec117c965d72f
Durrell Jules <dujules23@gmail.com>
2021-01-11 22:41:02 -0500

    project complete

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,22 +2,28 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-
 import styled from 'styled-components'
 
 
 const Container = styled.div`
     text-align: center
+    
 `
 
 const CharacterN = styled.h2`
     text-decoration: underline
     font-size: 3em
-    text-shadow: 2px 2px 5px grey
+    text-shadow: 2px 2px 5px red
+    border: 3px black;
+    border-radius:10px;
+    
 
 
 `
+// const CharacterCard = styled.div`
+//     border: 1px solid white;
 
+// `
 
 const Character = props => {
     
@@ -34,18 +40,17 @@ const Character = props => {
 
    })
      
-
+//when writing jsx, put javascript in curly braces {}.
 
     return(
 
         <Container className="App">
             <h1 className="Header">Characters from "Star Wars"</h1>
-               <div>
-               {names.map( (name, index) => 
-                   <CharacterN key={index}>{name}</CharacterN>)}
-      
-      
-               </div> 
+            
+               
+                {names.map( (name, index) => 
+                   <CharacterN tag="h2" key={index}>{name}</CharacterN>)}
+               
 
         </Container>
 
